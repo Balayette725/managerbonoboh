915c53d42cce67ee482e7fe03ac67958b719a001
Balayette725 <61594466+Balayette725@users.noreply.github.com>
2020-06-03 16:20:17 +0200

    Add files via upload

---
+++ manager/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meza sim</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap1.css">
+	<link rel="stylesheet" type="text/css" href="css/style1.css">
+</head>
+
+<header >
+	<div class="bg-grey1">
+		<div style="height: 80px;">
+
+			<img src="image/Logo_MezaSim.png" class=" float-left">
+
+			<div class="float-right italic text-white flex1">
+				<a href="#" class="text-white">Login</a>/<a href="#" class="text-white">Création de compte</a>
+				<img src="image/ReseauxSociaux.png">
+			</div>
+
+			<div style="padding-left: 44%;" class="flex2">
+				<p class="italic text-muted" >En route pour le web 3D</p>
+			</div>
+
+			<div class="float-right flex3" style="margin-top: -10px">
+				<img src="image/Logo_WebManager.png">
+			</div>
+
+			<div style="text-align: center;" class="flex4">
+				<a href="#"><img src="image/MENU.png"></a>
+			</div>
+
+		</div>
+	</div>
+</header>
+
+<body>
+	<div >
+		<div class="bg-greyop bandeau"></div>
+		<p class="greentext">Créez votre propre environnement virtuel "Web en 3D"</p>
+
+		<img src="image/Fond_PourAvatarAccueil.png" class="image1" style="width: 100%; height: 85%;">
+		<img src="image/AvatarsAccueil.png" class="image2">
+		<div class="centered bg-lightop" style="width: 100%; height: 47%"></div>
+		<div class="bandeau2 bg-greyop"></div>
+		<span class="orangetext"><p>Libre/Opensource, Décentralisé & Interconnecté</p></span>
+	</div>
+
+	<footer>
+		<div class="bg-grey1">
+			<p>Pour être maintenu MezaSim A BESOIN DE VOS DONS >>> Entreprises, associations, administrations, établissements d'enseignement DEMANDEZ NOS PROGRAMMES DE TRANSITIONS NUMERIQUE ET COLLABORATIONS</p>
+			<img src="image/CreativeCommons.png" alt="chackal" class="image3">
+		</div>
+
+	</footer>
+</body>
+</html>
